4cd1b1401d37120c429bb3c78e32eabf92ec4703
Sandip <sandipramanik07@gmail.com>
2021-11-23 01:36:26 +0530

    Add validations for signup route

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { body } from "express-validator";
 
 import { signinController } from "../controllers/auth/signin";
 import { signupController } from "../controllers/auth/signup";
@@ -8,7 +9,17 @@ import { currentUserController } from "../controllers/auth/current-user";
 const router = express.Router();
 const authUrl = "/api/v1/users";
 
-router.post(`${authUrl}/signup`, signupController);
+router.post(
+  `${authUrl}/signup`,
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 to 20 characters"),
+  ],
+  signupController
+);
 
 router.post(`${authUrl}/signin`, signinController);
 router.post(`${authUrl}/signout`, signoutController);
