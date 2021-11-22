74752d60d6c795fa4c58941fec6a92b3863e53b1
Sandip <sandipramanik07@gmail.com>
2021-11-22 20:09:55 +0530

    Add initial setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
