9f92462fcb56f89ff8b437bae6e9b46b5879ecc9
Sandip <sandipramanik07@gmail.com>
2021-11-23 20:37:39 +0530

    add .env variables and connect to mongodb atlas

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import "express-async-errors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 import { authRouter } from "./routes/auth.routes";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -17,6 +21,17 @@ app.use(errorHandler);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}!`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Cnnected to MongoDB...");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`);
+  });
+};
+
+start();
