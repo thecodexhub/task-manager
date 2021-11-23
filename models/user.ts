77e00d2b38ed4e7e70710881b7d1e2b68f2663cd
Sandip <sandipramanik07@gmail.com>
2021-11-23 20:37:49 +0530

    Add user model

---
+++ models/user.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+// An interface that describes the properties
+// that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
