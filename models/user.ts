27afc7de822b70779f25f4a2692ec8bf1735b00b
Sandip <sandipramanik07@gmail.com>
2021-11-24 00:33:18 +0530

    Add pre-save hook to hash password

--- models/user.ts
+++ models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { PasswordManager } from "../services/password-manager";
 
 // An interface that describes the properties
 // that are required to create a new user
@@ -30,6 +31,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await PasswordManager.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
