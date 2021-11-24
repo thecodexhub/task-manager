66e8957e39d059ff5b2a0cb9a2d655e2d6c90127
Sandip <sandipramanik07@gmail.com>
2021-11-24 11:36:40 +0530

    Add request validation middlewares to the routes

--- controllers/auth/signup.ts
+++ controllers/auth/signup.ts
@@ -1,18 +1,10 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
 import { User } from "../../models/user";
 import jwt from "jsonwebtoken";
 
-import { RequestValidationError } from "../../errors/request-validation-error";
 import { BadRequestError } from "../../errors/bad-request-error";
 
 export const signupController = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
-
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -6,6 +6,8 @@ import { signupController } from "../controllers/auth/signup";
 import { signoutController } from "../controllers/auth/signout";
 import { currentUserController } from "../controllers/auth/current-user";
 
+import { validateRequest } from "../middlewares/validate-request";
+
 const router = express.Router();
 const authUrl = "/api/v1/users";
 
@@ -18,10 +20,23 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 to 20 characters"),
   ],
+  validateRequest,
   signupController
 );
 
-router.post(`${authUrl}/signin`, signinController);
+router.post(
+  `${authUrl}/signin`,
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  signinController
+);
+
 router.post(`${authUrl}/signout`, signoutController);
 router.get(`${authUrl}/currentuser`, currentUserController);
 
