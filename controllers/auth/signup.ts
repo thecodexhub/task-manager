c2cd566d95e7b59e07762b96b950461ffb575a47
Sandip <sandipramanik07@gmail.com>
2021-11-24 11:15:32 +0530

    Add signup functionality

--- controllers/auth/signup.ts
+++ controllers/auth/signup.ts
@@ -1,5 +1,37 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { User } from "../../models/user";
+import jwt from "jsonwebtoken";
 
-export const signupController = (req: Request, res: Response) => {
-  console.log("Hey this is signup controller");
+import { RequestValidationError } from "../../errors/request-validation-error";
+import { BadRequestError } from "../../errors/bad-request-error";
+
+export const signupController = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestError("Email already in use!");
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  // Generate JWT
+  const userJwt = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  res.status(201).send({ user, token: userJwt });
 };
