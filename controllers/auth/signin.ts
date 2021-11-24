74e08284518e6a40d7b009c73e5a8a1844c19974
Sandip <sandipramanik07@gmail.com>
2021-11-24 11:47:06 +0530

    Add signin functionality

--- controllers/auth/signin.ts
+++ controllers/auth/signin.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import { User } from "../../models/user";
+import jwt from "jsonwebtoken";
 
-export const signinController = (req: Request, res: Response) => {
-  console.log("Hey this is signin controller");
+import { PasswordManager } from "../../services/password-manager";
+import { BadRequestError } from "../../errors/bad-request-error";
+
+export const signinController = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    throw new BadRequestError("Invalid credentials");
+  }
+
+  const isPasswordMatching = await PasswordManager.compare(
+    existingUser.password,
+    password
+  );
+  if (!isPasswordMatching) {
+    throw new BadRequestError("Invalid credentials");
+  }
+
+  const userJwt = jwt.sign(
+    {
+      id: existingUser.id,
+      email: existingUser.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  res.status(200).send({ user: existingUser, token: userJwt });
 };
