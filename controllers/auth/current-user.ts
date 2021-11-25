69b67eec17128b5559eaeb501c2c450baf47e292
Sandip <sandipramanik07@gmail.com>
2021-11-25 11:53:15 +0530

    Add current-user functionality

--- controllers/auth/current-user.ts
+++ controllers/auth/current-user.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const currentUserController = (req: Request, res: Response) => {
-  console.log("Hey this is currentuser controller");
+  const token: string = req.headers["x-access-token"] as string;
+
+  if (!token) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 };
