f6de382c52cb8375e61f6df6d44887387756622b
Sandip <sandipramanik07@gmail.com>
2021-11-25 11:53:24 +0530

    Add signout functionality

--- controllers/auth/signout.ts
+++ controllers/auth/signout.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const signoutController = (req: Request, res: Response) => {
-  console.log("Hey this is signout controller");
+  res.status(200).send({ user: null, token: null });
 };
