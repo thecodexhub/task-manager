bbdcb87f5502e5037008fe8b8b2f15f62366b216
Sandip <sandipramanik07@gmail.com>
2021-11-30 21:08:57 +0530

    Add create task controller

--- controllers/tasks/create.ts
+++ controllers/tasks/create.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import { Task } from "../../models/task";
 
 export const createController = async (req: Request, res: Response) => {
-  res.send("Hey there, this is create task controller!");
+  const { title, description, startTime, finishTime } = req.body;
+
+  const task = Task.build({
+    title,
+    description,
+    startTime: new Date(startTime),
+    finishTime: new Date(finishTime),
+    userId: req.currentUser!.id,
+  });
+
+  await task.save();
+
+  res.status(201).send(task);
 };
