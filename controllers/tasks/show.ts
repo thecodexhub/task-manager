df5ade2e8101e0fd49a87ac82ac10c87e87d5492
Sandip <sandipramanik07@gmail.com>
2021-12-01 01:15:18 +0530

    Add functionality for show controller

--- controllers/tasks/show.ts
+++ controllers/tasks/show.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { NotFoundError } from "../../errors/not-found-error";
+import { Task } from "../../models/task";
 
 export const showController = async (req: Request, res: Response) => {
-  res.send("Hey there, this is show single task controller!");
+  const tasks = await Task.find({
+    id: req.params.id,
+    userId: req.currentUser!.id,
+  });
+
+  if (tasks.length == 0) {
+    throw new NotFoundError();
+  }
+
+  res.send(tasks[0]);
 };
