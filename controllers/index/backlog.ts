694e3ba993b239296ddb08b1fee5067e4743cbdf
Sandip <sandipramanik07@gmail.com>
2021-12-03 16:19:30 +0530

    fix: add `done` flag on query

--- controllers/index/backlog.ts
+++ controllers/index/backlog.ts
@@ -5,6 +5,7 @@ import { Task } from "../../models/task";
 export const backlogController = async (req: Request, res: Response) => {
   const tasks = await Task.find({
     userId: req.currentUser!.id,
+    done: false,
     finishTime: { $lte: new Date() },
   });
 
